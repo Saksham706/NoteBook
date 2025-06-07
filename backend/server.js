7a9281bc9ac6952a06940e04302275e773e27fe4
Saksham Agarwal <sakshamagarwal706@gmail.com>
2025-06-07 18:15:42 +0530

    Updating server js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ const userRoutes = require('./routes/userRoutes')
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 app.use(cors({
     origin: process.env.FRONTEND_URL
 }));
